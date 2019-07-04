10d818c5197e3d9069caa4d5e9674d4a89339a08
Yuvraj2300 <yuvraj_2300@hotmail.com>
2019-07-04 17:27:47 +0530

    1 file modified

--- lifting-state-up/Calculator.js
+++ lifting-state-up/Calculator.js
@@ -1,5 +1,12 @@
 import react, { Component } from 'react';
 
 export default class Calculator extends Component {
-  
+  render() {
+    return (
+      <div>
+        <TemperatureInput scale="c" />
+        <TemperatureInput scale="f" />
+      </div>
+    );
+  }
 }
